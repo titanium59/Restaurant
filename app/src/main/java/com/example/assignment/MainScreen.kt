620d02183eb3ef8a1f4c6bdffd4c4bb478c98dd4
utsav <utsavpathak59@gmail.com>
2025-02-11 11:56:08 +0530

    set up how each meal item should look like

--- app/src/main/java/com/example/assignment/MainScreen.kt
+++ app/src/main/java/com/example/assignment/MainScreen.kt
@@ -1,7 +1,11 @@
 package com.example.assignment
 
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.aspectRatio
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Text
@@ -9,7 +13,12 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import coil.compose.rememberAsyncImagePainter
 
 @Composable
 fun MainScreen(modifier: Modifier = Modifier){
@@ -22,18 +31,45 @@ fun MainScreen(modifier: Modifier = Modifier){
                 Text(text = "Loading" , modifier.align(Alignment.Center))
             }
             viewstate.error != null -> {
-                Text(text = "Error Occured" , modifier.align(Alignment.Center))
+                Text(text = "Error Occurred" , modifier.align(Alignment.Center))
             }
             else -> {
-                LazyColumn {
-                    items(viewstate.mealList) { meal ->
-                        Text("Meal: ${meal.strCategory}")
-                    }
-                    items(viewstate.drinkList) { drink ->
-                        Text("Drink: ${drink.strDrink}")
-                    }
-                }
+                MealScreen(categories = viewstate.mealList)
             }
         }
+    }
+}
+
+@Composable
+fun MealScreen(categories: List<MealCategory>){
+    LazyColumn {
+        items(categories){
+            meal->
+            mealItem(category = meal)
+        }
+    }
+}
+
+//How Each meal looks like
+@Composable
+fun mealItem(category: MealCategory){
+    Column(
+        modifier = Modifier
+            .padding(8.dp)
+            .fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally) {
+        Image(
+            painter = rememberAsyncImagePainter(model = category.strCategoryThumb),
+            contentDescription = null,
+            modifier = Modifier
+                .fillMaxSize()
+                .aspectRatio(1f)
+        )
+        Text(
+            text = category.strCategory,
+            color = Color.Black,
+            style = TextStyle(fontWeight = FontWeight.Bold),
+            modifier = Modifier.padding(top = 4.dp)
+        )
     }
 }
